f2d236c4fbb06a90c99138d40f63a22555a53b56
paulthanksgiving <paulthanksgiving2@gmail.com>
2026-02-14 02:19:08 +0100

    billing-service_test

---
+++ billing-service/src/test/java/com/jvittoni/billingservice/BillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvittoni.billingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
