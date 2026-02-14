b77a1673e533c599fbe98b6efdd5864b4a3baaf6
paulthanksgiving <paulthanksgiving2@gmail.com>
2026-02-14 02:25:02 +0100

    patient-service_exception

---
+++ patient-service/src/main/java/com/jvittoni/patientservice/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.jvittoni.patientservice.exception;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
+}
