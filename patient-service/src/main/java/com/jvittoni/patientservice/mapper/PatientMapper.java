bceaee2e8ae5cc7e6f9c42c67daf6532c20a616a
paulthanksgiving <paulthanksgiving2@gmail.com>
2026-02-14 02:30:03 +0100

    patient-service_mapper

---
+++ patient-service/src/main/java/com/jvittoni/patientservice/mapper/PatientMapper.java
@@ -0,0 +1,31 @@
+package com.jvittoni.patientservice.mapper;
+
+import com.jvittoni.patientservice.dto.PatientRequestDTO;
+import com.jvittoni.patientservice.dto.PatientResponseDTO;
+import com.jvittoni.patientservice.model.Patient;
+
+import java.time.LocalDate;
+
+public class PatientMapper {
+    public static PatientResponseDTO toDTO(Patient patient) {
+        PatientResponseDTO patientDTO = new PatientResponseDTO();
+        patientDTO.setId(patient.getId().toString());
+        patientDTO.setName(patient.getName());
+        patientDTO.setEmail(patient.getEmail());
+        patientDTO.setAddress(patient.getAddress());
+        patientDTO.setDateOfBirth(patient.getDateOfBirth().toString());
+
+        return patientDTO;
+    }
+
+    public static Patient toModel(PatientRequestDTO patientRequestDTO) {
+        Patient patient = new Patient();
+        patient.setName(patientRequestDTO.getName());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+
+        return patient;
+    }
+}
