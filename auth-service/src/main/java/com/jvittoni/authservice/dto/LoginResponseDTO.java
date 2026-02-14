e0d8fca5407d4919299831fce0cd276383800fcc
paulthanksgiving <paulthanksgiving2@gmail.com>
2026-02-14 02:14:59 +0100

    auth-service_responce

---
+++ auth-service/src/main/java/com/jvittoni/authservice/dto/LoginResponseDTO.java
@@ -0,0 +1,14 @@
+package com.jvittoni.authservice.dto;
+
+public class LoginResponseDTO {
+
+    private final String token;
+
+    public LoginResponseDTO(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
